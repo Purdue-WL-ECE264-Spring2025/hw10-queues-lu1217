c8061b9d0188b2861a6538adbc7580285be101ea
Kayden Lu <lu1090@purdue.edu>
2025-04-04 17:52:39 -0400

    queue.c

--- queue.c
+++ queue.c
@@ -2,13 +2,20 @@
 #include "tile_game.h"
 #include <stdlib.h>
 
-void enqueue(struct queue *q, struct game_state state) {
+typedef struct {
+    struct game_state state;
+    int moves;
+} queue_item;
+
+void enqueue(struct queue *q, struct game_state state, int moves) {
     if (!q) return;
     
     struct list_node *new_node = malloc(sizeof(struct list_node));
     if (!new_node) return;
     
-    new_node->value = serialize(state);
+    queue_item item = {state, moves};
+    new_node->value = serialize(item.state);
+    new_node->moves = moves;  // Store moves separately
     new_node->next = NULL;
 
     if (!q->data.head) {
@@ -22,13 +29,14 @@ void enqueue(struct queue *q, struct game_state state) {
     }
 }
 
-struct game_state dequeue(struct queue *q) {
+struct game_state dequeue(struct queue *q, int *moves) {
     if (!q || !q->data.head) {
         return (struct game_state){0};
     }
 
     struct list_node *front = q->data.head;
     struct game_state state = deserialize(front->value);
+    *moves = front->moves;  // Retrieve stored moves
     q->data.head = front->next;
     free(front);
     
@@ -57,14 +65,16 @@ int number_of_moves(struct game_state start) {
     if (is_solved_state(start)) return 0;
     
     struct queue q = { .data = { .head = NULL } };
-    enqueue(&q, start);
+    enqueue(&q, start, 0);
     
     // Simple visited tracking
     int visited[65536] = {0};
     visited[serialize(start) % 65536] = 1;
     
     while (q.data.head) {
-        struct game_state current = dequeue(&q);
+        int current_moves;
+        struct game_state current = dequeue(&q, &current_moves);
+        int next_moves = current_moves + 1;
         
         int directions[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
         for (int i = 0; i < 4; i++) {
@@ -80,20 +90,20 @@ int number_of_moves(struct game_state start) {
                 
                 next.empty_row = new_r;
                 next.empty_col = new_c;
-                next.moves++;
                 
                 if (is_solved_state(next)) {
                     // Clean up queue
                     while (q.data.head) {
-                        dequeue(&q);
+                        int dummy;
+                        dequeue(&q, &dummy);
                     }
-                    return next.moves;
+                    return next_moves;
                 }
                 
                 size_t hash = serialize(next) % 65536;
                 if (!visited[hash]) {
                     visited[hash] = 1;
-                    enqueue(&q, next);
+                    enqueue(&q, next, next_moves);
                 }
             }
         }
