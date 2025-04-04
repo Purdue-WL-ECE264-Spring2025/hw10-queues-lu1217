0c9e6526fd8f50ffb4cbdeda26a425b425e3a83e
Kayden Lu <lu1090@purdue.edu>
2025-04-04 19:13:51 -0400

    queue.c

--- queue.c
+++ queue.c
@@ -2,66 +2,14 @@
 #include "tile_game.h"
 #include <stdlib.h>
 
-#define VISIT_SIZE (1 << 20)  // Sufficient for all test cases
+#define VISIT_SIZE (1 << 22)  // Increased size
+#define MAX_MOVES 50
 
-// Enhanced hash function that handles all positions
-size_t enhanced_hash(struct game_state state) {
-    size_t hash = 0;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            hash = (hash << 4) | state.tiles[i][j];
-            hash ^= (state.empty_row << 8) | state.empty_col;
-        }
-    }
-    return hash;
-}
-
-void enqueue(struct queue *q, struct game_state state) {
-    if (!q) return;
-    
-    struct list_node *new_node = malloc(sizeof(struct list_node));
-    if (!new_node) return;
-    
-    new_node->value = serialize(state);
-    new_node->next = NULL;
-
-    if (!q->data.head) {
-        q->data.head = new_node;
-    } else {
-        struct list_node *current = q->data.head;
-        while (current->next) {
-            current = current->next;
-        }
-        current->next = new_node;
-    }
-}
-
-struct game_state dequeue(struct queue *q) {
-    static struct game_state empty_state = {0};
-    if (!q || !q->data.head) {
-        return empty_state;
-    }
-
-    struct list_node *front = q->data.head;
-    struct game_state state = deserialize(front->value);
-    q->data.head = front->next;
-    free(front);
-    
-    return state;
-}
-
-int is_solved(struct game_state state) {
-    unsigned char expected = 1;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (i == 3 && j == 3) {
-                if (state.tiles[i][j] != 0) return 0;
-            } else {
-                if (state.tiles[i][j] != expected++) return 0;
-            }
-        }
-    }
-    return 1;
+size_t pattern_hash(struct game_state s) {
+    size_t h = 0;
+    for (int i = 0; i < 16; i++) 
+        h = (h << 4) | s.tiles[i/4][i%4];
+    return h ^ (s.empty_row << 8) ^ s.empty_col;
 }
 
 int number_of_moves(struct game_state start) {
@@ -70,48 +18,42 @@ int number_of_moves(struct game_state start) {
     if (!visited) return -1;
     
     int moves = 0;
-    
     if (is_solved(start)) {
         free(visited);
         return 0;
     }
 
     enqueue(&q, start);
-    visited[enhanced_hash(start) % VISIT_SIZE] = 1;
+    visited[pattern_hash(start) % VISIT_SIZE] = 1;
     
-    while (q.data.head) {
+    while (q.data.head && moves <= MAX_MOVES) {
         int level_size = 0;
-        struct list_node *current_node = q.data.head;
-        while (current_node) {
+        for (struct list_node *n = q.data.head; n; n = n->next) 
             level_size++;
-            current_node = current_node->next;
-        }
         
         while (level_size--) {
             struct game_state curr = dequeue(&q);
-            
             if (is_solved(curr)) {
                 free(visited);
                 while (q.data.head) dequeue(&q);
                 return moves;
             }
-            
-            int directions[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+
+            // Priority moves: right, down, left, up
+            int dirs[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
             for (int i = 0; i < 4; i++) {
-                int new_r = curr.empty_row + directions[i][0];
-                int new_c = curr.empty_col + directions[i][1];
-                
-                if (new_r >= 0 && new_r < 4 && new_c >= 0 && new_c < 4) {
+                int r = curr.empty_row + dirs[i][0];
+                int c = curr.empty_col + dirs[i][1];
+                if (r >= 0 && r < 4 && c >= 0 && c < 4) {
                     struct game_state next = curr;
-                    // More robust tile swap
-                    next.tiles[curr.empty_row][curr.empty_col] = next.tiles[new_r][new_c];
-                    next.tiles[new_r][new_c] = 0;
-                    next.empty_row = new_r;
-                    next.empty_col = new_c;
+                    next.tiles[curr.empty_row][curr.empty_col] = next.tiles[r][c];
+                    next.tiles[r][c] = 0;
+                    next.empty_row = r;
+                    next.empty_col = c;
                     
-                    size_t hash = enhanced_hash(next) % VISIT_SIZE;
-                    if (!visited[hash]) {
-                        visited[hash] = 1;
+                    size_t h = pattern_hash(next) % VISIT_SIZE;
+                    if (!visited[h]) {
+                        visited[h] = 1;
                         enqueue(&q, next);
                     }
                 }
