1f3e2896b51580aca088b14949a30811fcdfe5ed
Kayden Lu <lu1090@purdue.edu>
2025-04-04 20:00:33 -0400

    linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,14 +1,17 @@
 #include "linked_list.h"
 #include <stdlib.h>
 
+// Creates a new node with given value
 struct list_node *new_node(size_t value) {
     struct list_node *node = malloc(sizeof(struct list_node));
     if (!node) return NULL;
+    
     node->value = value;
     node->next = NULL;
     return node;
 }
 
+// Adds new node at beginning of list
 void insert_at_head(struct linked_list *list, size_t value) {
     struct list_node *node = new_node(value);
     if (!node) return;
@@ -17,6 +20,7 @@ void insert_at_head(struct linked_list *list, size_t value) {
     list->head = node;
 }
 
+// Adds new node at end of list
 void insert_at_tail(struct linked_list *list, size_t value) {
     struct list_node *node = new_node(value);
     if (!node) return;
@@ -32,6 +36,7 @@ void insert_at_tail(struct linked_list *list, size_t value) {
     }
 }
 
+// Removes and returns first node's value
 size_t remove_from_head(struct linked_list *list) {
     if (!list->head) return 0;
     
@@ -40,27 +45,24 @@ size_t remove_from_head(struct linked_list *list) {
     
     list->head = old_head->next;
     free(old_head);
-    
     return value;
 }
 
+// Removes and returns last node's value
 size_t remove_from_tail(struct linked_list *list) {
     if (!list->head) return 0;
     
     size_t value;
     
     if (!list->head->next) {
-        // Only one node
         value = list->head->value;
         free(list->head);
         list->head = NULL;
     } else {
-        // Find second-to-last node
         struct list_node *current = list->head;
         while (current->next->next) {
             current = current->next;
         }
-        
         value = current->next->value;
         free(current->next);
         current->next = NULL;
@@ -69,6 +71,7 @@ size_t remove_from_tail(struct linked_list *list) {
     return value;
 }
 
+// Frees all nodes in the list
 void free_list(struct linked_list list) {
     struct list_node *current = list.head;
     while (current) {
